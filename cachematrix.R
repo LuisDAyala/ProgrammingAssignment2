e6a7798748c81e6bafce562b9a0eacd77e9c8ba1
Luis Ayala <ldayalab@gmail.com>
2020-10-08 11:17:00 -0500

    This is the assignment 2 of the R programing course in Coursera platform.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        Inv<- NULL
+        setMatx <- function(l){
+                x<<-l
+                Inv<<- NULL
+        }
+        getMatx<- function()x
+        setInv<- function(MatxInv) Inv <<- MatxInv
+        getInv <- function() Inv
+        
+        list(setMatx = setMatx, getMatx=getMatx,
+             setInv=setInv, getInv=getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        Inv<-x$getInv()
+        if (!is.null(Inv)){
+                message("getting cached data")
+                return(Inv)
+        }
+        data<-x$getMatx()
+        Inv<-solve(data, ...)
+        x$setInv(Inv)
+        Inv
         ## Return a matrix that is the inverse of 'x'
 }
